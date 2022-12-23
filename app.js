7403d5189f013237ea65031073d6b5aa70bff5bd
Alexandra Dzygal <sdzygal@gmail.com>
2022-12-23 14:49:06 -0700

    Add success and failure roats, test app

--- app.js
+++ app.js
@@ -38,14 +38,24 @@ app.post("/", function (req, res) {
                 }
         
         const request = https.request(url, options, function(response) {
+
+              if (response.statusCode === 200) {
+                res.sendFile(__dirname + "/success.html");
+              } else {
+                res.sendFile(__dirname + "/failure.html");
+              }
                response.on("data", function(data) {
                         console.log(JSON.parse(data));
-               })
-        })
+               });
+        });
 
         request.write(jsonData);
         request.end();
 
+});
+
+app.post("/failure", function(req, res) {
+       res.redirect("/");
 });
 
 
@@ -53,9 +63,3 @@ app.listen(3000, function () {
 
 console.log(`Example app listening on 3000 port!`)
 });
-
-// API Key
-// cd4fb9f379637a3408f9b51a8c68ec09-us10
-
-// List ID
-// d44ddd3d4d
