bd56d9f5b6d08eed2fb1edd2098406a98293f2c1
Alexandra Dzygal <sdzygal@gmail.com>
2022-12-23 13:55:14 -0700

    Comlete mailchimp API settings

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require("body-parser");
 const request = require("request");
+const https = require("https");
 
 const app = express();
 
@@ -12,11 +13,39 @@ res.sendFile(__dirname + '/index.html');
 });
 
 app.post("/", function (req, res) {
-        var firstName = req.body.fName;
-        var lastName = req.body.lName;
-        var email = req.body.email;
+        const firstName = req.body.fName;
+        const lastName = req.body.lName;
+        const email = req.body.email;
+
+        const data = { 
+            members: [
+                {
+                        email_address: email, 
+                        status: "subscribed",
+                        merge_fields: {
+                              FNAME: firstName,
+                              LNAME: lastName  
+                        }
+                }
+            ]
+        };
+
+        const jsonData = JSON.stringify(data);
+        const url = "https://us10.api.mailchimp.com/3.0/lists/d44ddd3d4d";
+        const options = {
+                        method: "POST",
+                        auth: "alex1:cd4fb9f379637a3408f9b51a8c68ec09-us10"
+                }
         
-console.log(firstName, lastName, email);
+        const request = https.request(url, options, function(response) {
+               response.on("data", function(data) {
+                        console.log(JSON.parse(data));
+               })
+        })
+
+        request.write(jsonData);
+        request.end();
+
 });
 
 
@@ -24,3 +53,9 @@ app.listen(3000, function () {
 
 console.log(`Example app listening on 3000 port!`)
 });
+
+// API Key
+// cd4fb9f379637a3408f9b51a8c68ec09-us10
+
+// List ID
+// d44ddd3d4d
